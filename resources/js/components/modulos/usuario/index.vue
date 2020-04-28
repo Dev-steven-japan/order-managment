537dcd65085634fa175d0adde4f9a2d648ac3326
= <=>
2020-04-28 15:07:37 -0500

    Subir Proyecto Inicial

---
+++ resources/js/components/modulos/usuario/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Usuario</h1>
+                    </div>
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
