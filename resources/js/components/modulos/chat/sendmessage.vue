2c9b1563385784360c376e67c120b8f1d41a9b2e
= <=>
2020-08-14 16:41:01 -0500

    Incorporar Emojis al Chat

--- resources/js/components/modulos/chat/sendmessage.vue
+++ resources/js/components/modulos/chat/sendmessage.vue
@@ -1,15 +1,23 @@
 <template>
     <form action="#" method="post">
         <div class="input-group">
+            <button type="button" @click="toogleDialogEmoji" :readonly="(!contacto)?true:false">😃</button>
             <input type="text" v-model="message" @keydown.enter.prevent="setRegistrarMensaje" @keydown="escribiendoMensaje" placeholder="Escriba un mensaje..." class="form-control" :readonly="(!contacto)?true:false">
             <span class="input-group-append">
                 <button type="button" class="btn btn-primary" @click.prevent="setRegistrarMensaje" :readonly="(!contacto)?true:false">Enviar</button>
             </span>
         </div>
+        <VEmojiPicker
+            v-show="showDialog"
+            labelSearch="Search"
+            lang="pt-BR"
+            @select="onSelectEmoji"
+        />
     </form>
 </template>
 
 <script>
+    import VEmojiPicker, { emojisDefault, categoriesDefault } from "v-emoji-picker";
     export default {
         props: {
             contacto: {
@@ -20,9 +28,13 @@
                 type: Object
             }
         },
+        components: {
+            VEmojiPicker
+        },
         data() {
             return {
-                message: ''
+                message: '',
+                showDialog: false
             }
         },
         methods: {
@@ -55,7 +67,17 @@
                         contacto:   this.contacto.id,
                         typing  :   true
                     })
-            }
+            },
+            onSelectEmoji(emoji) {
+                console.log(emoji)
+                this.message += emoji.data
+            },
+            toogleDialogEmoji() {
+                if (!this.contacto) {
+                    return
+                }
+                this.showDialog = !this.showDialog;
+            },
         },
     }
 </script>
